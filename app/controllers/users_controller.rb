dea76f68f0c996018f91e961e04a9dd9fb337f92
nju520 <hwbnju@gmail.com>
2015-10-13 20:04:09 +0800

    modify user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,19 @@ class UsersController < ApplicationController
 
 	# 其实这样也可以使用new action,只是名字的不同而己，其他的都类似。
   def signup
-  	@user = User.new
+  	@user = User.new  #每当一次请求调用一个Controller时，Rails框架会实例化出一个Controller的实例。
+                      #这个实例中的实例变量的值需要在返回之前被设置成我们想要的值以便推送给Templates。
   end
 
   def create
   	@user = User.create(user_params) # 为了保证能把错误信息能够传递给html模版，这里必须使用实例变量。
   	if @user.save
       cookies[:auth_token] = @user.auth_token # 如果用户注册成功，直接去设置cookie，让她登陆成功
-  	redirect_to :root
+  	  redirect_to :root
   	else
-  	render 'signup'  # 不能为redirect_to
-  	end		
-  end	
+  	  render 'signup'  # 不能为redirect_to
+  	end
+  end
 
   #def create_login_session
   	#@user = User.find_by_name(params[:name])
@@ -51,9 +52,9 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)	
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     # params.require(:user).permit! 这样也可以，就是允许 user 的所有字段。
   end
-  #Parameters: {"utf8"=>"✓", "authenticity_token"=>"0CO40ESRVmU7YFAHpSy8Z7VdGjHNZqgB516AlxYbJBN4hNK2wFcVA3UrW+/1He4I5vRz2MWws/MY/K4q/QGP2w==", 
+  #Parameters: {"utf8"=>"✓", "authenticity_token"=>"0CO40ESRVmU7YFAHpSy8Z7VdGjHNZqgB516AlxYbJBN4hNK2wFcVA3UrW+/1He4I5vRz2MWws/MY/K4q/QGP2w==",
   #              "user"=>{"name"=>"韩文波", "email"=>"hwbnju@gmail.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"注册"}
 end
